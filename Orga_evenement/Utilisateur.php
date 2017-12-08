f77e9cf169ea4395b109ebf618658eb19c7c0ad3
k.dupeyrat <k.dupeyrat@protonmail.com>
2017-12-07 20:22:00 -0500

    commit à 2h21

---
+++ Orga_evenement/Utilisateur.php
@@ -0,0 +1,99 @@
+<html>
+
+<head>
+<meta charset="utf-8" />
+<style>
+.Texte{
+	text-align:right;
+	margin-left: 20%;
+	margin-right: 20%;
+	font-size:20;
+	color:white;
+	font-weight: bold;
+	width:60%;
+    background-color: rgba(0,0,0,0.3);
+}
+table {
+ border-width:3px; 
+ border-style:solid; 
+ border-color:white;
+ width:100%;
+ }
+ td { 
+ border-width:3px;
+ border-style:solid; 
+ border-color:white;
+ }
+</style>
+</head>
+
+<body>
+<br><br>
+<div class=Texte>
+	<table>
+		<tr>
+			<td width=40% rowspan=2>
+			<img src="" alt"Photo profil">
+			</td>
+			<td width=60%>
+			Pseudo
+			</td>
+		</tr>
+		<tr>
+			<td width=60%>
+			Paramètre du compte
+			</td>
+		</tr>
+	</table>
+</div>
+<br>
+<div class=Texte>
+	<table>
+		<tr>
+			<td width=30%>
+			Nombre de fois déjà SAM:
+			</td>
+			<td width=40%>
+			réponse
+			</td>
+			<td width=30% rowspan=5>
+			<img src="" alt"Photo voiture">
+			</td>
+		</tr>
+		<tr>
+			<td width=30%>
+			Nom et prénom:
+			</td>
+			<td width=40%>
+			réponse
+			</td>
+		</tr>
+		<tr>
+			<td width=30%>
+			Adresse:
+			</td>
+			<td width=40%>
+			réponse
+			</td>
+		</tr>
+		<tr>
+			<td width=30%>
+			Voiture:
+			</td>
+			<td width=40%>
+			réponse
+			</td>
+		</tr>
+		<tr>
+			<td width=30%>
+			Contact par E-mail:
+			</td>
+			<td width=40%>
+			réponse
+			</td>
+		</tr>
+	</table>
+</div>
+</body>
+
+</html>
